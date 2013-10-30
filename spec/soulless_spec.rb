15a4db54286031d4727aaf36be747e7aca882780
Anthony Smith <anthony@sticksnleaves.com>
2013-10-30 13:59:17 -0400

    Rounded out spec coverage.

--- spec/soulless_spec.rb
+++ spec/soulless_spec.rb
@@ -15,4 +15,21 @@ describe Soulless do
     @dummy_class.save
     @dummy_class.saved.should be_true
   end
+  
+  it 'should not call #persist! if attributes are invalid' do
+    @dummy_class.name = nil
+    @dummy_class.save.should be_false
+    @dummy_class.saved.should be_false
+  end
+  
+  it '#update_attributes should update multiple attributes and then save' do
+    @dummy_class.update_attributes(name: 'Yaw', email: 'yokoono@thebeatles.com')
+    @dummy_class.name.should == 'Yaw'
+    @dummy_class.email.should == 'yokoono@thebeatles.com'
+    @dummy_class.saved.should be_true
+  end
+  
+  it '#persisted? should be false' do
+    @dummy_class.persisted?.should be_false
+  end
 end
--- spec/support/dummy_class.rb
+++ spec/support/dummy_class.rb
@@ -2,6 +2,7 @@ class DummyClass
   include Soulless.model
   
   attribute :name, String, default: 'Anthony'
+  attribute :email, String
   attribute :saved, Boolean, default: false
 
   validates :name, presence: true
