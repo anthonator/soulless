e9d872b142ff71c6a5f6d280d6005e328350b76f
Anthony Smith <anthony@sticksnleaves.com>
2013-11-06 13:33:18 -0500

    Removed unused puts.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,4 @@ require 'soulless'
 
 # Require supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| puts f; require f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
