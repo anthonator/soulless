e64861a1fb5a18c3e84e9ba3e635c7a68e4650f7
Anthony Smith <anthony@sticksnleaves.com>
2013-11-07 00:13:02 -0500

    Cleaned up dummy class.

--- spec/support/dummy_class.rb
+++ spec/support/dummy_class.rb
@@ -4,20 +4,6 @@ class DummyClass
   attribute :name, String, default: 'Anthony'
   attribute :email, String
   attribute :saved, Boolean, default: false
-  
-  has_one :spouse do
-    attribute :name, String
-  end
-  
-  has_one :dummy_clone, DummyClass do
-    validates :email, presence: true
-  end
-  
-  has_many :friends do
-    attribute :name, String
-  end
-  
-  has_many :dummy_clones, DummyClass
 
   validates :name, presence: true
   
