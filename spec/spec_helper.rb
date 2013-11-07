3fcd320916ee6b5c569066eb6c3afb188c2ed952
Anthony Smith <anthony@sticksnleaves.com>
2013-11-06 19:22:53 -0500

    Required database_cleaner.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@
 require File.expand_path("../../spec/dummy/config/environment", __FILE__)
 
 require 'soulless'
+require 'database_cleaner'
 
 # Require supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
