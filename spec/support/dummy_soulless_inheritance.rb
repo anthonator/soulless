c57198773c98df06e1181c82d042390a630f6241
Anthony Smith <anthony@sticksnleaves.com>
2014-07-17 18:49:44 -0400

    Attempt to fix travis.

--- spec/support/dummy_soulless_inheritance.rb
+++ spec/support/dummy_soulless_inheritance.rb
@@ -1,8 +1,24 @@
 class DummySoullessInheritance
   include Soulless.model
-  
+
   inherit_from(::DummyClass, { exclude: :email })
-  
+
+  private
+  def persist!
+    self.saved = true
+  end
+end
+
+# This is a hack for now. For some reason Travis can't see dummy_class.rb.
+class DummyClass
+  include Soulless.model
+
+  attribute :name, String, default: 'Anthony'
+  attribute :email, String
+  attribute :saved, Boolean, default: false
+
+  validates :name, presence: true
+
   private
   def persist!
     self.saved = true
