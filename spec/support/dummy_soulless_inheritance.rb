654f8d25a8aa233538ea4db2fcce38fd4c708e78
Anthony Smith <anthony@sticksnleaves.com>
2014-07-29 19:15:14 -0400

    Removed model class from inheritance spec.

--- spec/support/dummy_soulless_inheritance.rb
+++ spec/support/dummy_soulless_inheritance.rb
@@ -8,19 +8,3 @@ class DummySoullessInheritance
     self.saved = true
   end
 end
-
-# This is a hack for now. For some reason Travis can't see dummy_class.rb.
-class DummyClass
-  include Soulless.model
-
-  attribute :name, String, default: 'Anthony'
-  attribute :email, String
-  attribute :saved, Boolean, default: false
-
-  validates :name, presence: true
-
-  private
-  def persist!
-    self.saved = true
-  end
-end
